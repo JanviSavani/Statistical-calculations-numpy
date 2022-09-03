9eda0658ddc3d32815e178a288b3741668a8cc72
Janvi Savani <91655692+JanviSavani@users.noreply.github.com>
2022-09-03 16:07:58 +0000

    Calculated Mean, Variance, Standard Deviation, Max, Min and Sum of 3x3 Matrix

--- main.py
+++ main.py
@@ -1,29 +1,51 @@
 import numpy as np
-from statistics import mean
 
 lm = [0,1,2,3,4,5,6,7,8]
 
 def calculate(l):
   m = np.array(lm).reshape((3, 3))
   print(m)
-  
+
+  #Mean
   rowmean = m.mean(0)
-  print("Row Mean: ", rowmean)
-
   colmean = m.mean(1)
-  print("Col Mean: ", colmean)
+  flattenedmean = m.mean()
 
-  rowvariance = np.var(m, axis=0)
-  print(rowvariance)
+  #Variance
+  rowvar = np.var(m, axis=0)
+  colvar = np.var(m, axis=1)
+  flattenedvar = np.var(m)
 
-  colvariance = np.var(m, axis=1)
-  print(colvariance)
+  #Standard Deviation
+  rowstd = np.std(m, axis=0)
+  colstd = np.std(m, axis=1)
+  flattenedstd = np.std(m)
+
+  #Max
+  rowmax = m.max(0)
+  colmax = m.max(1)
+  flattenedmax = m.max()
+
+  #Min
+  rowmin = m.min(0)
+  colmin = m.min(1)
+  flattenedmin = m.min()
+
+  #Sum
+  rowsum = m.sum(0)
+  colsum = m.sum(1)
+  flattenedsum = m.sum()
 
   d = {
-    'mean': [list(rowmean), list(colmean)],
-    'variance': [list(rowvariance), list(colvariance)]
+    'mean': [list(rowmean), list(colmean), flattenedmean],
+    'variance': [list(rowvar), list(colvar), flattenedvar],
+    'standard deviation': [list(rowstd), list(colstd), flattenedstd],
+    'max': [list(rowmax), list(colmax), flattenedmax],
+    'min': [list(rowmin), list(colmin), flattenedmin],
+    'sum': [list(rowsum), list(colsum), flattenedsum]
   }
-
-  print(d)
   
-calculate(lm)
+  return d
+  
+result = calculate(lm)
+print(result)
